59773f35ee8c99897e373f70d7a2e6d433dd67df
chsky1600 <ethanchankowsky@gmail.com>
2024-05-13 21:46:51 -0400

    fixed sendMessage

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
-// Create a listener for tab URL changes
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Check if the tab URL contains "google.ca/maps/dir" and "3e1"
-    if (tab.url.includes("google.ca/maps/dir") && tab.url.includes("3e1")) {
+    if (tab.url.includes("google.com/maps/dir/") && tab.url.includes("3e1")) {
+        console.log(tab.url);
         const queryParams = tab.url.split("?")[1];
         // const urlParams = new UrlSearchParams(queryParams);
-
+        
         chrome.tabs.sendMessage(tabId, {
             type: "NEW_ROUTE",
             routeId: "route"
         });
-        console.log("Cycling route option is active!");
         // Add your code here to handle the active cycling route option
     }
 });
+
+
+
--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,24 @@
+
 (() => {
-    let map; // The Google Map object
-    let directionsService; // The Google Maps Directions Service for route planning
-    let startLocation; // The start location for the route
-    let endLocation; // The end location for the route
-    let waypoints = []; // An array of waypoints for the route
-    let travelMode = 'BICYCLING'; // The travel mode for the route
+let map; // The Google Map object
+let directionsService; // The Google Maps Directions Service for route planning
+let startLocation; // The start location for the route
+let endLocation; // The end location for the route
+let waypoints = []; // An array of waypoints for the route
+let travelMode = 'BICYCLING'; // The travel mode for the route
 
-    chrome.runtime.onMessage.addListener((obj, sender, res) => {
-        const {type, routeId} = obj;
-
-        if (type === "NEW_ROUTE") {
-            currentRoute = routeId;
-            console.log("Cycling route option is active!");
-            findBestRoute();
-        };
-    });
-
-    findBestRoute = () => {
-        console.log("Finding the best route for cycling...");
+chrome.runtime.onMessage.addListener((obj, sender, res) => {
+    const {type, routeId} = obj;
+    console.log("made it this far")
+    if (obj.type === "NEW_ROUTE") {
+        currentRoute = routeId;
+        console.log("Cycling route option is active!");
+        findBestRoute();
     };
+});
+
+findBestRoute = () => {
+    console.log("Finding the best route for cycling...");
+};
 
 })();
